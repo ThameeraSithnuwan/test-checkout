4bfab2ee02f019bf8098cf52f48c71751d5bac99
ThameeraSithnuwan <149369868+ThameeraSithnuwan@users.noreply.github.com>
2024-08-09 11:28:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React main v2
+          Learn React test 2
         </a>
       </header>
     </div>
